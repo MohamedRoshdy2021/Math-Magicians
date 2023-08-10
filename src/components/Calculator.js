1121f71fff1f08c211f24c030b23efed819d2782
Mohammed roshdy <xmedox20111@gmail.com>
2023-08-10 20:16:38 +0300

    Added some childern components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,31 +7,48 @@ function Calucaltor() {
       <div className="nav">
         <p className="nav-p">0</p>
       </div>
-      <div className="math-opertion-right">
-        <p className="math-o-right">1</p>
-        <p className="math-o-right">1</p>
-        <p className="math-o-right">1</p>
-        <p className="math-o-right">1</p>
-        <p className="math-o-right">1</p>
-      </div>
-      <div className="math-opertion-top">
-        <p className="math-o-top">1</p>
-        <p className="math-o-top">1</p>
-        <p className="math-o-top">1</p>
-      </div>
-      <div className="math-numbers-con">
-        <p className="math-number">1</p>
-        <p className="math-number">2</p>
-        <p className="math-number">3</p>
-        <p className="math-number">4</p>
-        <p className="math-number">5</p>
-        <p className="math-number">6</p>
-        <p className="math-number">7</p>
-        <p className="math-number">8</p>
-        <p className="math-number">9</p>
-        <p className="math-number">0</p>
-        <p className="math-number">.</p>
-      </div>
+      <MathRight />
+      <MathTop />
+      <MathNumbers />
+    </div>
+  );
+}
+function MathRight() {
+  return (
+    <div className="math-opertion-right">
+      <p className="math-o-right">1</p>
+      <p className="math-o-right">1</p>
+      <p className="math-o-right">1</p>
+      <p className="math-o-right">1</p>
+      <p className="math-o-right">1</p>
+    </div>
+  );
+}
+
+function MathTop() {
+  return (
+    <div className="math-opertion-top">
+      <p className="math-o-top">1</p>
+      <p className="math-o-top">1</p>
+      <p className="math-o-top">1</p>
+    </div>
+  );
+}
+
+function MathNumbers() {
+  return (
+    <div className="math-numbers-con">
+      <p className="math-number">1</p>
+      <p className="math-number">2</p>
+      <p className="math-number">3</p>
+      <p className="math-number">4</p>
+      <p className="math-number">5</p>
+      <p className="math-number">6</p>
+      <p className="math-number">7</p>
+      <p className="math-number">8</p>
+      <p className="math-number">9</p>
+      <p className="math-number">0</p>
+      <p className="math-number">.</p>
     </div>
   );
 }
