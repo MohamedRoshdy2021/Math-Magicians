df637a5ab3b38256bff732c39fc6b1a062cea4ea
Mohammed roshdy <xmedox20111@gmail.com>
2023-08-10 20:11:04 +0300

    Added the Design for the application

---
+++ src/components/Calculator.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../index.css';
+
+function Calucaltor() {
+  return (
+    <div className="parentdiv">
+      <div className="nav">
+        <p className="nav-p">0</p>
+      </div>
+      <div className="math-opertion-right">
+        <p className="math-o-right">1</p>
+        <p className="math-o-right">1</p>
+        <p className="math-o-right">1</p>
+        <p className="math-o-right">1</p>
+        <p className="math-o-right">1</p>
+      </div>
+      <div className="math-opertion-top">
+        <p className="math-o-top">1</p>
+        <p className="math-o-top">1</p>
+        <p className="math-o-top">1</p>
+      </div>
+      <div className="math-numbers-con">
+        <p className="math-number">1</p>
+        <p className="math-number">2</p>
+        <p className="math-number">3</p>
+        <p className="math-number">4</p>
+        <p className="math-number">5</p>
+        <p className="math-number">6</p>
+        <p className="math-number">7</p>
+        <p className="math-number">8</p>
+        <p className="math-number">9</p>
+        <p className="math-number">0</p>
+        <p className="math-number">.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Calucaltor;
